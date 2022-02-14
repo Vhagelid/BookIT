d61d000ade592ca0a91e34c91a4cb33c1e3ca76f
ZG-RY0Ka <91533702+ZG-RY0Ka@users.noreply.github.com>
2022-02-14 11:50:23 +0100

    added tables width css

--- master/static/index.php
+++ master/static/index.php
@@ -7,6 +7,83 @@
     <title>Document</title>
 </head>
 <body>
-    
+<div class="container">
+  <div class="Booking">
+  <table style="width:100%">
+    <tr>
+        <th>BookingSystem</th>
+        <th>8:00-8:45</th>
+        <th>8:45-9:30</th>
+        <th>9:30-10:15</th>
+        <th>10:15-11:00</th>
+        <th>11:00-11:45</th>
+    </tr>
+    <tr>
+        <td>Mandag</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Vilmer Baking</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+    </tr>
+    <tr>
+        <td>Tirsdag</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+    </tr>
+    <tr>
+        <td>Onsdag</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+    </tr>
+        <tr>
+        <td>Torsdag</td>
+        <td>Tomt</td>
+        <td>Even, Intercourse</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+    </tr>
+    <tr>
+        <td>Fredag</td>
+        <td>Tomt</td>
+        <td>Tomt</td>    
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+    </tr>
+        <tr>
+        <td>Lørdag</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+    </tr>
+    <tr>
+        <td>Søndag</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+        <td>Tomt</td>
+    </tr>
+</table>
+  </div>
+  <div class="Header"></div>
+  <div class="Footer"></div>
+  <div class="footer"></div>
+  <div class="header"></div>
+  <div class="booking"></div>
+  <div class="main"></div>
+  <div class="mainL"></div>
+  <div class="mainR"></div>
+</div>
 </body>
 </html>
