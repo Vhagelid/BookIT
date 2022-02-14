d568aaf8b12c4f7fb505b98d738ea2f99064a4b9
Ev1M <90681485+Ev1M@users.noreply.github.com>
2022-02-14 20:20:05 +0100

    DB conn

---
+++ master/static/dbh.php
@@ -0,0 +1,10 @@
+<?php
+
+      $server = "localhost";
+      $username = "root";
+      $password = "";
+      $dbname = "dbBookIT";
+
+      $conn = mysqli_connect($server, $username, $password, $dbname);
+
+      ?>
